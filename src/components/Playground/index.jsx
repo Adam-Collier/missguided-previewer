2a7083bf472a82847629c5775bb345806e703c9e
Adam Collier <adamcollierart@gmail.com>
2021-08-03 12:27:15 +0100

    feature: switch between form and editor view

--- src/components/Playground/index.jsx
+++ src/components/Playground/index.jsx
@@ -4,13 +4,10 @@ import { LiveProvider, LiveError, LivePreview } from 'react-live';
 import { mdx as mdxLib } from '@mdx-js/react';
 import * as missguidedComponents from 'missguided-components';
 import * as creativeComponents from 'vite-storybook';
-import {
-  Container,
-  Row,
-  Col,
-} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import { Section } from '../Section';
+import { SectionToggle } from '../SectionToggle';
 import { Highlight } from '../Highlight';
 import { FormEditor } from '../FormEditor';
 import { MDXEditor } from '../MDXEditor';
@@ -24,13 +21,16 @@ import s from './playground.module.css';
 const Playground = ({ live }) => {
   const [isSDK, setIsSDK] = useState(false);
   const [sdk, setSDK] = useState(undefined);
+  const [view, setView] = useState('forms');
   // toggle between form and json view
 
   // update data and layout seperately
   // join together only when sending to amplience
   const [json, setJSON] = useState('{}');
-  const [mdx, setMDX] = useState('');
+  // const [mdx, setMDX] = useState('');
 
+  // keep our ast data here
+  // this acts as our source of truth between the mdx and form
   const [ast, setAST] = useState({});
 
   const [previewMDX, setPreviewMDX] = useState('');
@@ -47,9 +47,9 @@ const Playground = ({ live }) => {
         sdk.field.getValue().then((data) => {
           console.log('field data:', data, typeof data);
           // setContent(JSON.parse(data));
-          let allData = JSON.parse(data);
-          setJSON(allData.data);
-          setMDX(allData.layout);
+          // let allData = JSON.parse(data);
+          // setJSON(allData.data);
+          // setMDX(allData.layout);
         });
       })
       .catch((err) => {
@@ -68,13 +68,6 @@ const Playground = ({ live }) => {
     return () => clearTimeout(timeOutId);
   }, [json]);
 
-  useEffect(() => {
-    const timeOutId = setTimeout(() => {
-      setPreviewMDX(mdx);
-    }, 1000);
-    return () => clearTimeout(timeOutId);
-  }, [mdx]);
-
   if (live) {
     return (
       <LiveProvider
@@ -92,30 +85,35 @@ const Playground = ({ live }) => {
       >
         <div className={s.wrapper}>
           <section className={s.sidebar}>
-            <Section>
-              <Amplience
-                isSDK={isSDK}
-                sdk={sdk}
-                json={json}
-                mdx={mdx}
-                setJSON={setJSON}
-                setMDX={setMDX}
-              />
-            </Section>
-            <Section text="form">
-              <FormEditor ast={ast} setAST={setAST} components={getComponentAttributes(ast)}/>
-            </Section>
-            <Section text="mdx">
+            <Amplience
+              isSDK={isSDK}
+              sdk={sdk}
+              json={json}
+              mdx={previewMDX}
+              setJSON={setJSON}
+              setMDX={setPreviewMDX}
+            />
+            <SectionToggle view={view} setView={setView} />
+            {/* hide or show the editor or forms via a class (this way we maintain state throughout) */}
+            <FormEditor
+              className={view === 'forms' ? "" : s.hidden}
+              ast={ast}
+              setAST={setAST}
+              components={getComponentAttributes(ast)}
+            />
+            <div
+              className={`${s.mdxEditor} ${view === 'editor' ? "" : s.hidden}`}
+            >
               <MDXEditor
                 ast={ast}
-                setMDX={setMDX}
+                setPreviewMDX={setPreviewMDX}
                 setAST={setAST}
                 className={s.mdx}
               />
-            </Section>
-            <Section text="error">
-              <LiveError className={s.error} />
-            </Section>
+              <Section text="error">
+                <LiveError className={s.error} />
+              </Section>
+            </div>
           </section>
           <LivePreview className={s.preview} />
         </div>
@@ -123,7 +121,7 @@ const Playground = ({ live }) => {
     );
   }
 
-  return <Highlight code={mdx} />;
+  return <Highlight code={previewMDX} />;
 };
 
 export { Playground };
