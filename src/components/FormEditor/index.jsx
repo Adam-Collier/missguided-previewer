2a0e9fcf2358227110424434b4985521204471dc
Adam Collier <adamcollierart@gmail.com>
2021-08-03 12:26:11 +0100

    feature: FormEditor more performant

--- src/components/FormEditor/index.jsx
+++ src/components/FormEditor/index.jsx
@@ -2,42 +2,68 @@ import { useState, useEffect } from 'react';
 import { getComponentAttributes } from '../../lib/component-attributes';
 // import { prettyPrint } from 'recast';
 import { editTree } from '../../lib/edit-tree';
+import s from './formeditor.module.css';
+import Accordion from '../Accordion';
+import { Text, Stack } from 'vite-storybook';
 
-export const FormEditor = ({ ast, setAST, handleAST }) => {
+export const FormEditor = ({ ast, setAST, className }) => {
   const [components, setComponents] = useState([]);
+  const [formAST, setFormAST] = useState(ast);
 
   useEffect(() => {
     setComponents(getComponentAttributes(ast));
   }, [ast]);
 
+  useEffect(() => {
+    setComponents(getComponentAttributes(formAST));
+  }, [formAST]);
+
+  // debounce how often setAST gets updated
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      setAST(formAST);
+    }, 250);
+    return () => clearTimeout(timeOutId);
+  }, [formAST]);
+
   let handleChange = (e, position) => {
-    let updatedAST = editTree(ast, e.target.value, position);
-    setAST({ ...updatedAST });
+    setFormAST({ ...editTree(ast, e.target.value, position) });
   };
 
-  return components.map((component, index) => {
+  return (
+    <div className={`${s.wrapper} ${className || ''}`}>
+      {components.map((component, index) => {
+        let { name, props } = component;
 
-    let { name, props } = component;
+        // generate the forms
+        // when we edit the input change the ast
+        // re render the forms
 
-    // generate the forms
-    // when we edit the input change the ast
-    // re render the forms
-
-    return (
-      <div key={index}>
-        <p>{name}</p>
-        {Object.entries(props).map(([name, prop], index) => (
-          <label htmlFor={name} key={index}>
-            {name}
-            <input
-              type={prop.type}
-              name={name}
-              value={prop.value}
-              onChange={(e) => handleChange(e, prop.position)}
-            />
-          </label>
-        ))}
-      </div>
-    );
-  });
+        return (
+          <Accordion title={name} key={index} className={s.form}>
+            <Stack gap={0.25} className={s.content}>
+              {Object.entries(props).map(([name, prop], index) => (
+                <Stack
+                  as="label"
+                  gap={0.25}
+                  htmlFor={name}
+                  key={index}
+                  className={s.label}
+                >
+                  <Text size="sm">{name}</Text>
+                  <input
+                    className={s.input}
+                    type={prop.type}
+                    name={name}
+                    value={prop.value}
+                    onChange={(e) => handleChange(e, prop.position)}
+                  />
+                </Stack>
+              ))}
+            </Stack>
+          </Accordion>
+        );
+      })}
+    </div>
+  );
 };
