0bcba8d13590a6e9a70f6a3a6b22ce17a8d303c5
Adam Collier <adamcollierart@gmail.com>
2021-08-03 12:29:14 +0100

    chore: handle empty strings

--- src/lib/component-attributes.js
+++ src/lib/component-attributes.js
@@ -4,7 +4,7 @@ export const getComponentAttributes = (ast) => {
   let components = [];
 
   visit(ast, (node) => {
-    if(!node) return;
+    if (!node) return;
 
     if (node.type === 'JSXOpeningElement') {
       // get the component name from the opening tags
@@ -15,18 +15,24 @@ export const getComponentAttributes = (ast) => {
       if (node.attributes) {
         // loop over each prop and get its name and value
         node.attributes.forEach((attribute) => {
-          if (!attribute.value) return {};
+          if (!attribute.value) return;
 
-          console.log(node);
-
+          // grab the value from the attributes
           let value = attribute.value.expression
-              ? attribute.value.expression.value
-              : attribute.value.value;
+            ? attribute.value.expression.value
+            : attribute.value.value;
 
+          // if no value is found skip this iteration
+          // but keep empty string values as we should be able to edit these
+          if(value === undefined) return; 
+
+          // add the prop to the object
           componentProps[attribute.name.name] = {
             value,
             type: typeof value,
-            position: attribute.value.expression ? attribute.value.expression.start: attribute.value.start,
+            position: attribute.value.expression
+              ? attribute.value.expression.start
+              : attribute.value.start,
           };
         });
       }
@@ -36,8 +42,7 @@ export const getComponentAttributes = (ast) => {
         props: componentProps,
       });
     }
-
-  })
+  });
 
   return components;
 };
